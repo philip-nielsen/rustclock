c8a8b90048e069a319963992c3c0534ba768d960
Sea_Raider <philipnielsen64@gmail.com>
2023-03-18 23:59:42 +0100

    progress?

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
 mod utils;
 
-use std::panic;
-use std::time::SystemTime;
 use chrono::{DateTime, Local};
+use std::time::SystemTime;
+use std::{collections::btree_map::Range, panic};
 
 use console_error_panic_hook;
 
-use web_sys::{Document, Element};
 use wasm_bindgen::prelude::*;
+use web_sys::{Document, Element};
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -21,7 +21,7 @@ pub fn run() {
 }
 
 #[wasm_bindgen]
-extern {
+extern "C" {
     fn alert(s: &str);
 }
 
@@ -29,8 +29,6 @@ extern {
 pub fn greet() {
     alert("Hello, rustclock2!");
 }
-
-
 
 pub fn get_binary_time() -> [u64; 6] {
     let local_time: DateTime<Local> = Local::now();
@@ -55,36 +53,61 @@ pub fn output_binary_clock(binary_time: [u64; 6]) {
     let document = web_sys::window().unwrap().document().unwrap();
     let container = document.get_element_by_id("container").unwrap();
 
-    let width = web_sys::window().unwrap().inner_width().unwrap().as_f64().unwrap();
+    let width = web_sys::window()
+        .unwrap()
+        .inner_width()
+        .unwrap()
+        .as_f64()
+        .unwrap();
     let circle_size = (width / 10.0) as u32;
     let num_rows = 3;
 
     container.set_inner_html(""); // clear the container before adding new circles
 
     // center the container horizontally and align it to the top
-    container.set_attribute("style", "display: flex; justify-content: center; align-items: flex-start; flex-wrap: wrap;").unwrap();
+    container
+        .set_attribute(
+            "style",
+            "display: flex; justify-content: center; align-items: flex-start; flex-wrap: wrap;",
+        )
+        .unwrap();
+    let normal_range = 0..2;
 
     for row in (0..num_rows) {
         let row_container = document.create_element("div").unwrap();
-        row_container.set_attribute("style", "display: flex;").unwrap();
+        row_container
+            .set_attribute("style", "display: flex;")
+            .unwrap();
 
+        let new_range = normal_range.clone().map(|x| x + row as u64);
         let num_circles = if row == 0 { 5 } else { 6 };
+        let bitRange = if row == 0 {
+            0..num_circles - 3
+        } else {
+            0..num_circles - 2
+        };
 
-        for col in 0..num_circles {
-            let circle = document.create_element("div").unwrap();
-            //Wrong, not working
-            let color = if (binary_time[row*2+col/3] & (1 << (col%3))) != 0 { "#2e2e2e;" } else if (col % 2 == 0) {"#96c"} else { "#f280a1" };
-            circle.set_attribute("style", &format!("background-color: {}; border-radius: 50%; width: {}px; height: {}px; display: inline-block; margin: {}px;", color, circle_size, circle_size, circle_size/5)).unwrap();
-            row_container.append_child(&circle).unwrap();
+        for i in new_range.clone() {
+            for j in bitRange.clone() {
+                let bit = (binary_time[i as usize] >> (3 - j)) & 1;
+
+                let circle = document.create_element("div").unwrap();
+                //Wrong, not working
+                let color = if bit != 0 {
+                    "#2e2e2e;"
+                } else if (j % 2 == 0) {
+                    "#96c"
+                } else {
+                    "#f280a1"
+                };
+                circle.set_attribute("style", &format!("background-color: {}; border-radius: 50%; width: {}px; height: {}px; display: inline-block; margin: {}px;", color, circle_size, circle_size, circle_size/5)).unwrap();
+                row_container.append_child(&circle).unwrap();
+            }
         }
 
         container.append_child(&row_container).unwrap();
     }
 }
-
-
-
-
 
 #[wasm_bindgen]
 pub fn get_and_output_binary_time() {
